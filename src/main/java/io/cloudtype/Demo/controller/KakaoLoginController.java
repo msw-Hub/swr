65d291a61678a7fa1481c86030633fac2481d585
msw-Hub <133941998+msw-Hub@users.noreply.github.com>
2024-03-11 14:25:21 +0900

    1.3

--- src/main/java/io/cloudtype/Demo/controller/KakaoLoginController.java
+++ src/main/java/io/cloudtype/Demo/controller/KakaoLoginController.java
@@ -8,10 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.Map;
@@ -30,6 +27,7 @@ public class KakaoLoginController {
     @Autowired
     private UserInfoService userInfoService; // UserInfoService 주입
 
+    @CrossOrigin(origins = "https://teamswr.store")
     @GetMapping("/callback")
     public ResponseEntity<String> callback(@RequestParam("code") String code, HttpSession session, HttpServletResponse response) throws IOException {
         log.info(code);
