280eb20bc92811ee7b43e8d57578f101bd343d4a
msw-Hub <133941998+msw-Hub@users.noreply.github.com>
2024-03-18 14:00:40 +0900

    1.6

--- src/main/java/io/cloudtype/Demo/controller/KakaoLoginController.java
+++ src/main/java/io/cloudtype/Demo/controller/KakaoLoginController.java
@@ -35,12 +35,13 @@ public class KakaoLoginController {
         String expiresInStr = tokens.get("expires_in");
         int expiresIn = Integer.parseInt(expiresInStr);
         String refreshToken = tokens.get("refresh_token");
-        String refreshTokenExpiresIn = tokens.get("refresh_token_expires_in");
+        String refreshTokenExpiresInStr = tokens.get("refresh_token_expires_in");
+        int refreshTokenExpiresIn = Integer.parseInt(refreshTokenExpiresInStr);
 
         log.info("Access Token : " + accessToken);
         log.info("Expires_In : " + expiresInStr);
         log.info("Refresh Token : " + refreshToken);
-        log.info("Refresh Token Expires In : " + refreshTokenExpiresIn);
+        log.info("Refresh Token Expires In : " + refreshTokenExpiresInStr);
 
         // 현재 시간을 가져옴
         Instant now = Instant.now();
@@ -48,6 +49,10 @@ public class KakaoLoginController {
         // 만료 시간을 현재 시간에 만료 기간을 더한 값으로 계산
         Instant expiresAtInstant = now.plusSeconds(expiresIn);
         long expiresAtUnix = expiresAtInstant.getEpochSecond();
+
+        // refreshToken 만료 시간을 계산
+        Instant refreshTokenExpiresAtInstant = now.plusSeconds(refreshTokenExpiresIn);
+        long refreshTokenExpiresAtUnix = refreshTokenExpiresAtInstant.getEpochSecond();
 
         log.info(String.valueOf(expiresAtUnix));
 
@@ -65,9 +70,8 @@ public class KakaoLoginController {
         Map<String, Object> jsonResponse = new HashMap<>();
         jsonResponse.put("access_token", accessToken);
         jsonResponse.put("expires_at_unix", expiresAtUnix);
-        jsonResponse.put("refresh_token",refreshToken);
-        jsonResponse.put("refresh_token_expires_in",refreshTokenExpiresIn);
-
+        jsonResponse.put("refresh_token", refreshToken);
+        jsonResponse.put("refresh_token_expires_in", refreshTokenExpiresAtUnix); // 수정된 부분
 
         // ObjectMapper를 사용하여 Map 객체를 JSON 문자열로 변환
         ObjectMapper objectMapper = new ObjectMapper();
