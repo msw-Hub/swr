17e261dcb80a878ed65e673b857d43179aa55d58
msw-Hub <133941998+msw-Hub@users.noreply.github.com>
2024-03-21 21:08:17 +0900

    1.8

--- src/main/java/io/cloudtype/Demo/controller/KakaoLoginController.java
+++ src/main/java/io/cloudtype/Demo/controller/KakaoLoginController.java
@@ -3,6 +3,10 @@ package io.cloudtype.Demo.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cloudtype.Demo.service.KakaoService;
 import io.cloudtype.Demo.service.UserInfoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,6 +21,7 @@ import java.util.Map;
 @Slf4j
 @RestController
 @RequestMapping("")
+@Api(tags = "Kakao Login")
 public class KakaoLoginController {
 
     @Value("${kakao.client_id}")
@@ -28,6 +33,10 @@ public class KakaoLoginController {
     @Autowired
     private UserInfoService userInfoService; // UserInfoService 주입
 
+    @ApiOperation(value = "카카오 콜백", notes = "카카오 인증 콜백 엔드포인트")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "카카오 인증이 성공적으로 처리되었습니다.")
+    })
     @CrossOrigin(origins = {"https://teamswr.store", "http://localhost:5173"})
     @GetMapping("/callback")
     public ResponseEntity<String> callback(@RequestParam("code") String code) throws IOException {
@@ -84,6 +93,10 @@ public class KakaoLoginController {
         return ResponseEntity.ok().body(jsonString);
     }
 
+    @ApiOperation(value = "액세스 토큰 갱신", notes = "액세스 토큰 갱신 엔드포인트")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "액세스 토큰이 성공적으로 갱신되었습니다.")
+    })
     @CrossOrigin(origins = {"https://teamswr.store", "http://localhost:5173"})
     @PostMapping("/refresh")
     public ResponseEntity<String> refresh(@RequestBody Map<String, String> requestBody) throws IOException {
@@ -128,6 +141,12 @@ public class KakaoLoginController {
         return ResponseEntity.ok().body(jsonString);
     }
 
+    @ApiOperation(value = "회원가입", notes = "회원가입을 위한 엔드포인트입니다. 클라이언트는 유저 토큰을 헤더에 포함하여 요청을 보내야 합니다. 바디에는 폰 번호, 핀 번호, 생일을 포함해야 합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "회원가입이 성공적으로 처리되었습니다."),
+            @ApiResponse(code = 400, message = "입력값에 공백 문자가 포함되어 있거나 유효하지 않은 값입니다."),
+            @ApiResponse(code = 500, message = "내부 서버 오류가 발생했습니다.")
+    })
     @CrossOrigin(origins = {"https://teamswr.store", "http://localhost:5173"})
     @PostMapping("/signup")
     public ResponseEntity<String> signUp(@RequestHeader("Authorization") String accessToken,
