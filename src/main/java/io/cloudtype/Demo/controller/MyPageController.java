178489682f6d90fce6771769320d9a0e6cee61a8
msw-Hub <133941998+msw-Hub@users.noreply.github.com>
2024-03-26 10:25:04 +0900

    1.10

--- src/main/java/io/cloudtype/Demo/controller/MyPageController.java
+++ src/main/java/io/cloudtype/Demo/controller/MyPageController.java
@@ -95,7 +95,11 @@ public class MyPageController {
             // 받은 핀 번호와 데이터베이스의 핀 번호 비교
             Map<String, Object> jsonResponse = new HashMap<>();
             if (pinNumber.equals(dbPinNumber)) {
+                String nickname = (String) userInfo.get("nickname");
+                String phoneNumber = (String) userInfo.get("phone_number");
                 jsonResponse.put("success", "핀번호가 일치함");
+                jsonResponse.put("nickname", nickname);
+                jsonResponse.put("phone_number", phoneNumber);
                 ObjectMapper objectMapper = new ObjectMapper();
                 String jsonString = objectMapper.writeValueAsString(jsonResponse);
                 return ResponseEntity.ok().body(jsonString);
